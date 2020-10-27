56841237180902bc4a792e54add6892a27c50ce7
nsrecc <noel.recchia@gmail.com>
2020-10-26 17:43:04 -0700

    Setup base environment variables

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,8 +1,11 @@
 import React from 'react';
 import Head from 'next/head';
+import Link from 'next/link';
 import styles from '../styles/Home.module.css';
 
 export default function Home() {
+  const { NEXT_PUBLIC_FILMYTE_CLIENT_URL } = process.env;
+
   return (
     <div className={styles.container}>
       <Head>
@@ -12,6 +15,12 @@ export default function Home() {
 
       <main className={styles.main}>
         <h1 className={styles.title}>Welcome to Filmyte web server!</h1>
+        <h2>
+          <Link href="/api/graphql">Visit the GraphQL playground</Link>
+        </h2>
+        <h2>
+          <Link href={NEXT_PUBLIC_FILMYTE_CLIENT_URL}>Visit the Filmyte web client</Link>.
+        </h2>
       </main>
     </div>
   );
